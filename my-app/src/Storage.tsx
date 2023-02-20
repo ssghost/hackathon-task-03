adcae9e6b8d3016fd5eb6240f62e49eae5df827f
ssghost <ssprof0@gmail.com>
2023-02-20 22:47:51 +0800

    update storage

--- my-app/src/Storage.tsx
+++ my-app/src/Storage.tsx
@@ -1,63 +1,64 @@
 import { err, ok } from 'neverthrow';
 import { useCallback, useEffect, useState } from 'react';
 import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
-import { Hammer } from 'react-bootstrap-icons';
 import { resultFromTruthy } from './Contract';
 
 interface Props {
     canRecv: boolean;
-    receive: (value: number) => void;
+    receive: (should: boolean, value: number) => void;
 }
 
 export default function MyStorage(props: Props) {
     const { canRecv, receive } = props;
     const [inputValue, setInputValue] = useState('');
     const [recvValue, setRecvValue] = useState<number>();
+    const [should, setShould] = useState<boolean>(false);
     const [validationError, setValidationError] = useState<string>();
 
     useEffect(() => {
-        const [ivalue, error] = resultFromTruthy(inputValue, undefined)
+        const [ivalue, error]: [any, any] = resultFromTruthy(inputValue, undefined)
             .andThen((input: any) => {
-                const amount = Number(input);
-                return Number.isNaN(ivalue)) ? err('invalid input') : ok(ivalue);
+                const ivalue = Number(input);
+                return Number.isNaN(ivalue) ? err('invalid input') : ok(ivalue);
             })
             .match<[bigint?, string?]>(
-                (a: any) => [BigInt(Math.round(a * 1e6)), undefined],
+                (a: any) => [Number(a), undefined],
                 (e: any) => [undefined, e]
             );
-        setDepositAmount(amount);
+        setRecvValue(ivalue);
+        setShould(should);
         setValidationError(error);
-    }, [depositInput]);
+    }, [inputValue]);
 
-    const handleSubmitDeposit = useCallback(() => {
-        console.log(`Attempting to deposit ${depositAmount} uCCD.`);
-        if (depositAmount) {
-            deposit(depositAmount);
-            setDepositInput('');
+    const handleSubmitStorage = useCallback(() => {
+        console.log(`Attempting to update storage.`);
+        if (recvValue) {
+            receive(should, recvValue);
+            setShould(false);
+            setInputValue('');
         }
-    }, [depositAmount, deposit]);
+    }, [recvValue, receive]);
     return (
         <Row>
             <Form.Group as={Col} md={8}>
                 <InputGroup className="mb-3" hasValidation>
-                    <InputGroup.Text id="basic-addon1">CCD</InputGroup.Text>
+                    <InputGroup.Text id="basic-addon1">Storage</InputGroup.Text>
                     <Form.Control
                         type="text"
-                        placeholder="Amount to deposit"
-                        value={depositInput}
-                        onChange={(e) => setDepositInput(e.target.value)}
+                        placeholder="Value to Store:"
+                        value={inputValue}
+                        onChange={(e: any) => setInputValue(e.target.value)}
                         isInvalid={Boolean(validationError)}
                     />
-                    <Button variant="primary" onClick={handleSubmitDeposit} disabled={!canDeposit || !depositAmount}>
-                        Deposit
+                    <Form.Check type="checkbox" id="should-checkbox">
+                        <Form.Check.Input isValid value={should} onChange={() => setShould(!should)}></Form.Check.Input>
+                        <Form.Check.Label>You decide whether the value should be stored.</Form.Check.Label>    
+                    </Form.Check> 
+                    <Button variant="primary" onClick={handleSubmitStorage} disabled={!canRecv || !recvValue}>
+                        Update New Value
                     </Button>
                     <Form.Control.Feedback type="invalid">{validationError}</Form.Control.Feedback>
                 </InputGroup>
-            </Form.Group>
-            <Form.Group as={Col} md={4}>
-                <Button variant="danger" className="w-100" onClick={smash} disabled={!canSmash}>
-                    <Hammer />
-                </Button>
             </Form.Group>
         </Row>
     );
--- my-app/src/react-app-env.d.ts
+++ my-app/src/react-app-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react-scripts" />
 declare module '@concordium/react-components';
 declare module '@walletconnect/types';
+declare module 'react/jsx-runtime';
